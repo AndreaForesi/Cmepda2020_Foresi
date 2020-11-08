dc19aac90f313a24fd1947d3cb2b4725b222e369
andreaforesi1997@gmail.com <andreaforesi1997@gmail.com>
2020-11-08 19:54:33 +0100

    inserimento percentuale lettere maiuscole e minuscole,problemi con matplotlib

--- Assegnamento1_Foresi.py
+++ Assegnamento1_Foresi.py
@@ -21,45 +21,64 @@ import argparse
 import logging
 import time
 import string
-
-logging.basicConfig(level=logging.DEBUG)
+import matplotlib.pyplot as plt
 
 """ Read and analysis the file input_file and print charachter"""
 
-def process (input_file):
+def process (input_file, lower=True, upper=False, histo=False):
 
     start_time = time.time()
 
-    logging.info('\n Reading the file %s..\n', input_file)
+    logging.info("Reading the file %s..\n", input_file)
 
     with open(input_file) as file:
 
-        #for text in file.readline:
-        text = file.read()
+        text = file.read() #pratica ridefinizione
 
-        Char_enum = {ch: 0 for ch in string.ascii_lowercase}
-        #Char_enum = {ch: 0 for ch in string.ascii_uppercase}
-        #Char_enum = {ch: 0 for ch in string.ascii_letters}
+            #Ricerca lettere
+        if lower:
+            logging.info("Ricerca lettere minuscole..")
+            Char_enum = {ch: 0 for ch in string.ascii_lowercase}
+            for ch in text:
+                if ch in Char_enum:
+                    Char_enum[ch] += 1
+            TotLetter=sum(Char_enum.values())
+            for ch,num in Char_enum.items():
+                print(f"The number of '{ch}' in text {input_file} is {num} and is the {(num / TotLetter *100 ):.2f}%\n")
+            print(f"\nLettere minuscole totali {TotLetter}")
 
-        elapsed_time = start_time - time.time()
+        if upper:
+            logging.info("Ricerca lettere maiuscole..")
+            Char_enum = {ch: 0 for ch in string.ascii_uppercase}
+            for ch in text:
+                if ch in Char_enum:
+                    Char_enum[ch] += 1
+            TotLetter=sum(Char_enum.values())
+            for ch,num in Char_enum.items():
+                print(f"The number of '{ch}' in text {input_file} is {num} and is the {(num / TotLetter *100 ):.2f}%\n")
+            print(f"\nLettere maiuscole totali {TotLetter}")
 
-        logging.info('\n Done in %.4f \n', elapsed_time)
+        #if histo:
 
 
-        print(f'there is {Char_enum}')
-
+            #time
+        elapsed_time = -start_time + time.time()
+        print(f"\nDone in {elapsed_time:.3f} \n")
 
 
 if __name__=='__main__':
-
-    #logging.info('Il file è eseguito come main, inizializzo il parser')
-    parser = argparse.ArgumentParser()          #inizializzazione analizzatore
-
+          #inizializzazione analizzatore
+    parser = argparse.ArgumentParser()
         #Aggiunte argomenti al parser
     parser.add_argument('infile', type = str , help = 'path to input file')
+    parser.add_argument('-debug', action='store_true' , help='Print debugger messages')
+    parser.add_argument('-lower', action='store_false' , help = 'search the lowercase letters')
+    parser.add_argument('-upper', action='store_true' , help = 'search the uppercase letters')
+    parser.add_argument('-histo', action='store_true' , help = 'Show a digram of letter percentage' )
         #analizza il parser e assegna i campi ad args
     args = parser.parse_args()
-    #print(type(args),'\n')
-    file_path = args.infile
 
-    process(file_path)
+    if args.debug: #questo argmento non va dato alla funzione process
+        logging.basicConfig(level=logging.DEBUG)
+
+    process(args.infile, args.lower, args.upper, args.histo)
